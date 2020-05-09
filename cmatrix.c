b0f3e6ab49f9a8cd44508fae46a5d04eea783651
hectortav <tavhector@gmail.com>
2020-05-09 13:37:04 +0300

    Functions needed for neural network are ready

--- cmatrix.c
+++ cmatrix.c
@@ -35,6 +35,73 @@ void matrix_scalar_add(matrix m, double number) {
     return;
 }
 
+void matrix_hadamard(matrix m, matrix a) {
+    // element - wise mult
+    int i;
+    // more checks
+    if (m->rows != a->rows || m->columns != a->columns) {
+        printf("ERROR: matrices should have the save dimensions\n");
+        exit(1);
+    }
+    for (i = 0; i < m->rows * m->columns; i++) {
+        m->p[i] *= a->p[i];
+    }
+    return;
+}
+
+void matrix_add(matrix m, matrix a) {
+    int i;
+    // more checks
+    if (m->rows != a->rows || m->columns != a->columns) {
+        printf("ERROR: matrices should have the save dimensions\n");
+        exit(1);
+    }
+    for (i = 0; i < m->rows * m->columns; i++) {
+        m->p[i] += a->p[i];
+    }
+    return;
+}
+
+matrix matrix_mult(matrix m, matrix a) {
+    if (m->columns != a->rows) {
+        printf("ERROR:number of columns of the first matrix is different than the rows of the second one\n");
+        exit(1);
+    }
+    matrix n = matrix_constructor(m->rows, a->columns);
+    int i, j, k;
+    for (i = 0; i < n->rows; i++) {
+        for (j = 0; j < n->columns; j++) {
+            for (k = 0; k < m->columns; k++) {
+                n->p[i * n->columns + j] += m->p[i*m->columns + k] * a->p[k*a->columns + j];
+            }
+        }
+    }
+    return n;
+}
+
+double get_random(double min, double max) {
+    return (double)rand()/RAND_MAX*(max + 1.0) + min;
+}
+
+void matrix_randomize(matrix m, double min, double max) {
+    int i;
+    for (i = 0; i < m->rows * m->columns; i++) {
+        m->p[i] = get_random(min, max);
+    }
+    return;
+}
+
+matrix transpose(matrix m) {
+    int i, j;
+    matrix t = matrix_constructor(m->columns, m->rows);
+    for (i = 0; i < m->columns; i++) {
+        for (j = 0; j < m->rows; j++) {
+            t->p[i * m->rows * j] = m->p[i + j * m->columns];
+        }
+    }
+    return t;
+}
+
 void matrix_print(matrix m) {
     int i;
     if (m == NULL) {
@@ -50,13 +117,4 @@ void matrix_print(matrix m) {
         }
     }
     return;
-}
-
-
-int main(void) {
-    matrix m0 = matrix_constructor(3, 2);
-    matrix_scalar_add(m0, 1.3);
-    matrix_scalar_mult(m0, 2);
-    matrix_print(m0);
-    matrix_delete(m0);
 }
--- cmatrix.h
+++ cmatrix.h
@@ -1,6 +1,17 @@
-
 typedef struct { 
     int rows;
     int columns; 
     double *p;
 } _matrix, *matrix;
+
+matrix matrix_constructor(int, int);
+void matrix_delete(matrix);
+void matrix_scalar_mult(matrix, double);
+void matrix_scalar_add(matrix, double);
+void matrix_hadamard(matrix, matrix);
+void matrix_add(matrix, matrix);
+matrix matrix_mult(matrix, matrix);
+double get_random(double, double);
+void matrix_randomize(matrix, double, double);
+matrix transpose(matrix);
+void matrix_print(matrix);
